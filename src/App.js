075969f9b06585af01f966b6d9c0f6e27430ccb4
Chamith Lakshan <chamilucky8@gmail.com>
2022-11-30 12:10:21 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+import Login from './components/Login';
+import {BrowserRouter, BrowserRouter as Router,Route,Routes,Switch} from 'react-router-dom'
+import wlcm from './components/wlcm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+      <Route exact path="/" element={Login()}/>
+      <Route exact path="/dashboard" element={wlcm()}/>
+     </Routes>
     </div>
-  );
+  )
+
 }
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import {Button,Card,Input,Space,Form, Alert} from 'antd';
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
+
+function Login(){
+  const [uname, setUname] = useState("");
+  const [passwd, setPasswd] = useState("");
+  const navigate=useNavigate();
+
+  const obj = { 
+    "email":uname,
+    "password":passwd
+}
+
+const postfunc  = ()  =>  {
+  setUname(uname)
+  setPasswd(passwd)
+  axios.post("https://ujkp2xeahs.us-east-1.awsapprunner.com/api/v1/authenticate/login",obj)
+  .then(response => verify(response))
+  .catch(err => alert("user is invalid"))
+  console.log(obj)
+}
+  
+const verify = (response) => {
+  const httpcode=response.data.httpCode
+  if(httpcode==200){
+    navigate("/dashboard")
+  }
+  else{
+      return console.log("False")
+  }
+}
+
+    return(
+      <div id="Maindiv">
+        <div id="Subdiv1">
+          <h4>Login</h4>
+        </div>
+        <hr className="Solid"></hr>
+        <div id="Subdiv2">
+          <Form>
+            <Form.Item>
+            <lable className="Lbltxt">Email</lable>
+            <Input type="email" placeholder="input email" value={uname} onChange = {(e)=>setUname(e.target.value)} />
+            </Form.Item>
+            <Form.Item>
+            <lable className="Lbltxt">Passowrd</lable>
+            <Input.Password id="inp" placeholder="input password" value={passwd} onChange = {(e)=>setPasswd(e.target.value)} />
+            </Form.Item>
+            <Form.Item >
+            <Button htmlType = "submit" block id="Btn" onClick={postfunc}>Login</Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+    )
+}
+
+export default Login;
